cab878cec02980cb30baceb0e72b134991192f7d
Mohamed Chourafa <chourafamohamed@gmail.com>
2023-12-26 17:27:13 +0100

    first commit

---
+++ src/test/java/com/example/serveurgestionstockproduit/ServeurGestionStockProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serveurgestionstockproduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
